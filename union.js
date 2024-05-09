63feb3105063370264036d7c1bf7214eb18a577a
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-09 10:26:22 +0530

    union type

---
+++ union.js
@@ -0,0 +1,12 @@
+var myVal = "10";
+myVal = 44;
+function combine(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    else {
+        return a + ' ' + b;
+    }
+}
+console.warn(combine(5, 20));
+console.warn(combine("Abhinav", "Prakash"));
---
+++ union.ts
@@ -0,0 +1,21 @@
+let myVal: number | string = "10";
+
+myVal = 44;
+
+function combine(a:number | string, b:number|string){
+    if(typeof a === 'number' && typeof b === 'number'){
+        return a+b;
+    } else {
+        return a + ' ' + b;
+    }
+
+}
+
+console.warn(combine(5, 20));
+
+console.warn(combine("Abhinav", "Prakash"))
+
+
+
+
+
