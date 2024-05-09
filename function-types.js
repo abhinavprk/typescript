67aaf4707c79d75876455225c56a444389b6c48a
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-09 11:07:13 +0530

    js file

---
+++ function-types.js
@@ -0,0 +1,15 @@
+function add() {
+    return 6;
+}
+function myName() {
+    return 'abhinav';
+}
+function yourName() {
+    return;
+}
+function thisName() {
+}
+function thatName() {
+}
+function youName() {
+}
