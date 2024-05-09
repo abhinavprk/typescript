3e565bc43bb6868a47626d6ed0b44357de21cef3
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-09 11:06:22 +0530

    function return type

---
+++ function-types.ts
@@ -0,0 +1,21 @@
+
+function add():number {
+    return 6;
+}
+
+function myName():string|number {
+    return 'abhinav';
+}
+
+function yourName():undefined {
+    return;
+}
+
+function thisName():undefined {
+}
+
+function thatName() {
+}
+
+function youName():void {
+}
