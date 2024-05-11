dc0c934a8617dd34d2a450f37b00394995856c5c
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-11 16:50:44 +0530

    output dir

--- src/any.js
+++
@@ -1,5 +0,0 @@
-let data = 20;
-let anyVal = 20;
-anyVal = true;
-let stringAndNumber = "abhi";
-stringAndNumber = 10;
--- src/array.js
+++
@@ -1,7 +0,0 @@
-let arr = ['php', 'js', 'ts'];
-arr.push('net');
-arr[2] = 'roma';
-console.warn(arr);
-console.warn(arr.join(':'));
-let ran = ['php', 123, 'ts'];
-let arrVal = ['php', 123, 'ts'];
